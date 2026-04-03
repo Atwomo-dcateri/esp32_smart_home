45a123dbdf24d83abae5c202b1466fc9ed9e1473
Atwomo-dcateri <zggfbz@outlook.com>
2026-04-03 13:45:28 +0800

    完成网络层mqtt驱动开发

--- components/mqtt_handler/include/mqtt_handler.h
+++ components/mqtt_handler/include/mqtt_handler.h
@@ -1,1 +1,10 @@
-void func(void);
+#ifndef MQTT_HANDLER_H
+#define MQTT_HANDLER_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+void mqtt_app_start(void);
+void mqtt_send_sensor_data(float temp, float humi);
+
+#endif
--- components/mqtt_handler/mqtt_handler.c
+++ components/mqtt_handler/mqtt_handler.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
+#include <stdint.h>// 一般加上为好
+
+#include "mqtt_client.h"
+#include "esp_system.h"// 只要用到esp_xx.h文件加上准没错
+#include "esp_log.h"
+
 #include "mqtt_handler.h"
 
+static esp_mqtt_client_handle_t client;
 
-void func(void)
+void mqtt_app_start(void)
 {
-    
+    esp_mqtt_client_config_t mqtt_cfg = {
 
+        .broker.address.uri = "mqtt://broker.emqx.io",
+    };
+
+    client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_start(client);
+}
+
+void mqtt_send_sensor_data(float temp, float humi) {
+
+    char payload[128];
+    sprintf(payload, "{\"temperature\": %.1f, \"humidity\": %.1f}", temp, humi);
+    esp_mqtt_client_publish(client, "/my_home/sensor_data", payload, 0, 1, 0);
+    ESP_LOGI("MQTT_MGR", "Data sent: %s", payload);
 }
